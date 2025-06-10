cb8c99b62e8434b026ecf1a4727323f1fb637fcf
orangeisacid <1534563895@qq.com>
2025-06-10 10:59:37 +0800

    抽象Mono单例

---
+++ Assets/Scripts/Misc/SingletonMonobehaviour.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public abstract class SingletonMonobehaviour<T>:MonoBehaviour where T:MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType(typeof(T)) as T;
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+    
+    
+}
