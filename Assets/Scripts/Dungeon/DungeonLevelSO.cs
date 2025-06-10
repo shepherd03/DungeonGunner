94683c66866e45e29e0688765faeafb5d589491c
orangeisacid <1534563895@qq.com>
2025-06-10 10:29:10 +0800

    地牢等级asset

--- Assets/Scripts/Dungeon/DungeonLevelSO.cs
+++ Assets/Scripts/Dungeon/DungeonLevelSO.cs
@@ -49,17 +49,46 @@ public class DungeonLevelSO : ScriptableObject
 
             if (!isNSCorridor)
             {
-                Debug.Log($"{this.name.ToString()}中没有指定南北走廊");
+                Debug.Log($"{roomTemplate.name}中没有指定南北走廊");
             }
 
             if (!isEWCorridor)
             {
-                Debug.Log($"{this.name.ToString()}中没有指定东西走廊");
+                Debug.Log($"{roomTemplate.name}中没有指定东西走廊");
             }
 
             if (!isEntrance)
             {
-                Debug.Log($"{this.name.ToString()}中没有指定入口");
+                Debug.Log($"{roomTemplate.name}中没有指定入口");
+            }
+        }
+
+        foreach (var roomNodeGraph in roomNodeGraphList)
+        {
+            if (roomNodeGraph == null) return;
+            foreach (var roomNode in roomNodeGraph.roomNodeList)
+            {
+                if(roomNode == null) continue;
+
+                RoomNodeTypeSO roomNodeType = roomNode.roomNodeType;
+                if (roomNodeType.isEntrance || roomNodeType.isCorridorEW || roomNodeType.isCorridorNS ||
+                    roomNodeType.isCorridor || roomNodeType.isNone)
+                {
+                    continue;
+                }
+                
+                bool isRoomNodeTypeFound = false;
+
+                if (roomTemplateList.Find(roomTemplate => roomTemplate.roomNodeType == roomNodeType) != null)
+                {
+                    isRoomNodeTypeFound = true;
+                    break;
+                }
+
+                if (!isRoomNodeTypeFound)
+                {
+                    Debug.Log($"文件{this.name}:{roomNodeGraph.name}中缺少房间模板类型{roomNode.roomNodeType.roomNodeTypeName}");
+                }
             }
         }
     }
