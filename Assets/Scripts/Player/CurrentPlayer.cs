c73f2abfcf3a10989f686442b6ec0327934aa6f0
orangeisacid <1534563895@qq.com>
2025-06-19 14:01:28 +0800

    角色数据文件

--- Assets/Scripts/Player/CurrentPlayer.cs
+++ Assets/Scripts/Player/CurrentPlayer.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "CurrentPlayer", menuName = "ScriptableObjects/Player/Current Player")]
+[CreateAssetMenu(fileName = "CurrentPlayer", menuName = "Scriptable Objects/Player/Current Player")]
 public class CurrentPlayer : ScriptableObject
 {
     public PlayerDetailsSO playerDetails;
